b0e39b87369280d60ec74fb43490ae3c67a56a7b
Olimkhon Qodirov <olimkhondeveloper@gmail.com>
2020-10-20 19:02:19 +0300

    olim

--- olim_2/src/tj/olimhon/Main.java
+++ olim_2/src/tj/olimhon/Main.java
@@ -1,8 +1,16 @@
 package tj.olimhon;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] masiv = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+        for (int i = 0; i < masiv.length; i++) {
+            sum += masiv[i];
+        }
+        System.out.println(sum);
+
     }
 }
