c1e102b0d28bf5948113859248ed1d29faf809e8
Olimkhon Qodirov <olimkhondeveloper@gmail.com>
2020-10-20 18:44:17 +0300

    olimhon ilmmhona

---
+++ olim_1/src/tj/olimhon/Main.java
@@ -0,0 +1,13 @@
+package tj.olimhon;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int x = 5;
+	    int y = 6;
+
+	    int a = x + y;
+
+        System.out.println(a);
+    }
+}
